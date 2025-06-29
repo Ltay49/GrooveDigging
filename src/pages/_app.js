b667f066e9a78fa2933c706c806a9f901927d60f
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-06-30 00:20:24 +0100

    styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,16 @@
 import '@/styles/globals.css'; // or your global CSS path
-import { Space_Grotesk } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 
-const spaceGrotesk = Space_Grotesk({
+const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
   display: 'swap',
 });
 
+
 export default function App({ Component, pageProps }) {
   return (
-    <main className={spaceGrotesk.className}>
+    <main className={montserrat.className}>
       <Component {...pageProps} />
     </main>
   );
